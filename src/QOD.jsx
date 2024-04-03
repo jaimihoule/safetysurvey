fef3f625bf982b51da9da81251a1f837633b82f7
jaimihoule <jaimih.003@gmail.com>
2024-04-03 16:49:22 -0400

    add multiple questions

--- src/QOD.jsx
+++ src/QOD.jsx
@@ -7,24 +7,97 @@ export default function () {
     const questions = [{
                 type: "radiogroup",
                 name: "question1",
-                title: "What is the most common cause of lifeguard injuries?",
+                title: "What protective gear is NOT needed?",
                 choices: [
-                    "Slip and fall accidents", "Pool chemicals", "Sunburns", "Sharks"
+                    "Protective Eyeglasses", "Hard hat", "Protective Jacket", "Gas mask"
                 ],
-                correctAnswer: "Slip and fall accidents"
+                correctAnswer: "Gas mask"
             },
             {
                 type: "radiogroup",
                 name: "question2",
-                title: "How much sunscreen should you apply?",
+                title: "When should you NOT work?",
                 choices: [
-                    "teaspoon", "tablespoon", "quart", "gallon"
+                    "In the rain", "In extreme wind", "On a sunny day", "On a cloudly day"
                 ],
-                correctAnswer: "tablespoon"
-            }];
+                correctAnswer: "In extreme wind"
+            },
+            {
+                type: "radiogroup",
+                name: "question3",
+                title: "Which item should you bring to clean",
+                choices: [
+                    "Squeegee", "A rock", "Tap water", "Dish soap"
+                ],
+                correctAnswer: "Squeegee"
+            },
+            {
+                type: "radiogroup",
+                name: "question4",
+                title: "What extra items are you allowed to bring up?",
+                choices: [
+                    "Your computer", "Your phone", "Your Ipad", "Your rubix cube"
+                ],
+                correctAnswer: "Your phone"
+            },
+            {
+                type: "radiogroup",
+                name: "question5",
+                title: "What is a possible outcome of not following protocols?",
+                choices: [
+                    "Working quicker", "Nothing", "Having a fun time", "Safety Risks"
+                ],
+                correctAnswer: "Safety Risks"
+            },
+            {
+                type: "radiogroup",
+                name: "question6",
+                title: "How should your harness fit?",
+                choices: [
+                    "Very loose", "Slightly loose", "Comfortably snug", "Very tight"
+                ],
+                correctAnswer: "Comfortably snug"
+            },
+            {
+                type: "radiogroup",
+                name: "question7",
+                title: "If you suspect a malfunction in your harness you should...?",
+                choices: [
+                    "Continue working", "Get down safely", "Inspect in the air", "Do nothing"
+                ],
+                correctAnswer: "Get down safely"
+            },
+            {
+                type: "radiogroup",
+                name: "question8",
+                title: "Before you begin working you should?",
+                choices: [
+                    "Inspect your equipment", "Just start", "Do nothing", "Setup quickly"
+                ],
+                correctAnswer: "Inspect your equipment"
+            },
+            {
+                type: "radiogroup",
+                name: "question9",
+                title: "If your safety equipment is damaged you should...?",
+                choices: [
+                    "Repair equiment yourself", "Use damaged equipment", "Use no eqiupment", "Get new equipment"
+                ],
+                correctAnswer: ""
+            },
+            {
+                type: "radiogroup",
+                name: "question10",
+                title: "If it is extremely windy outside you should...?",
+                choices: [
+                    "Work a different day", "Work as normal", "Try to work quickly", "Use extra saftey gear"
+                ],
+                correctAnswer: "Work a different day"
+            }
+            ];
     const nQuestion = Math.floor((Math.random() * questions.length));
     const surveyJson = {
-        title: "Lifeguard Safety",
+        title: "Window Cleaning Safety",
         showCorrectAnswer: "always",
         showProgressBar: "bottom",
         firstPageIsStarted: true,
@@ -32,7 +105,7 @@ export default function () {
         pages: [{
             elements: [{
                 type: "html",
-                html: "You are about to start a quiz on Lifeguard Safety. <br>You will have 30 seconds for every question and 60 seconds to end the quiz.<br>Enter your name below and click <b>Start Quiz</b> to begin."
+                html: "You are about to start a quiz on Window Cleaning Safety. <br>You will have 30 seconds for every question and 60 seconds to end the quiz.<br>Enter your name below and click <b>Start Quiz</b> to begin."
             }, {
                 type: "text",
                 name: "username",
